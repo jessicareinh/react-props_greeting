b426d820090ad34795a8090feed1985bf1abde01
jessicareinh <148791371+jessicareinh@users.noreply.github.com>
2023-12-22 15:23:06 +0100

    add solution

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 import "./styles.css";
 
-export default function App() {
-  return <h1>Replace me with your component!</h1>;
+function Greeting({ name }) {
+  if (name === "Coach") {
+    return <h1>Hello, Coach!</h1>;
+  } else {
+    return <h1>Hello, {name}!</h1>;
+  }
 }
+
+function App() {
+  return (
+    <div className="App">
+      <Greeting name="Alice" />
+    </div>
+  );
+}
+
+export default App;
+
